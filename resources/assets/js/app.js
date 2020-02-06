30b2163d0c30047386d9a7873c18f29c596668cd
mgthihazaw <mgthihazaw777@gmail.com>
2020-02-06 14:29:33 +0630

    add js css scss to app.js using webpack with babel

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,13 @@
 require("./../css/app.css");
+require("./../sass/app.scss");
+
+// class App {
+//   constructor() {
+//     let data = "hello";
+//   }
+//   notify(message) {
+//     alert(message);
+//   }
+// }
+// let app = new App();
+// app.notify("Hello Welcome");
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,34 @@ var webpack = require("webpack");
 var path = require("path");
 
 module.exports = {
+  //from
   entry: ["./resources/assets/js/app.js"],
+  //to
   output: {
     path: path.resolve(__dirname, "./public/js"),
     filename: "app.js"
   },
+  //use module that we wanted
   module: {
     rules: [
       {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
       }
     ]
-  }
+  },
+  plugins: []
 };
+//minified version
+if (process.env.NDE_ENV == "production") {
+  module.exports.plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
