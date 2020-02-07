688d91f7a07559368013af5a577a1f12f40d4071
mgthihazaw <mgthihazaw777@gmail.com>
2020-02-07 14:14:52 +0630

    add DATABASE ORM

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -6,6 +6,10 @@ require_once __DIR__ . '/../app/config/_env.php';
 
 require_once __DIR__ . '/../app/routing/routes.php';
 
+//instantiate database class
+new \App\Classes\Database();
+
+
 
 // require_once __DIR__ . '/../app/routing/RouteDispatcher.php';
 
--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
-
+// phpinfo();
+// die();
 require_once '../bootstrap/init.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$user = Capsule::table('users')->where('id', 1)->first();
+var_dump($user->username);
