45ff427d3fe253701ec778b25202e47809cecc8f
mgthihazaw <mgthihazaw777@gmail.com>
2020-02-07 15:53:27 +0630

    add 404 page

--- public/index.php
+++ public/index.php
@@ -6,4 +6,4 @@ require_once '../bootstrap/init.php';
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $user = Capsule::table('users')->where('id', 1)->first();
-var_dump($user->username);
+echo "<br><br>Welcome To " . ucfirst($user->fullname);
